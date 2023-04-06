15472e654179012d2c8ac6c62ac5565d49ba448d
sumurillo <sumire.ohana@gmail.com>
2023-04-06 13:43:50 -0500

    add form for adding notes

--- src/pages/NewNotePage/NewNotePage.jsx
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 export default function NewNotePage() {
+  const [note, setNote] = useState('');
+
+  function handleChange(evt) {
+    setNote(evt.target.value)
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  }
+  
   return (
+    <>
     <h1>NewNotePage</h1>
+    <form action="">
+      <label htmlFor="note">Text:
+        <input type="text" name="note" id="text" onChange={handleChange} />
+      </label>
+      <input type="submit" value="Add Note" />
+    </form>
+    </>
   );
 }
