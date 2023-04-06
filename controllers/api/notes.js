c90b05bc79e3fcd9c0a99fc9a3def9affda52d35
sumurillo <sumire.ohana@gmail.com>
2023-04-06 14:45:12 -0500

    create a note

---
+++ controllers/api/notes.js
@@ -0,0 +1,21 @@
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// const User = require('../../models/user');
+const Note = require('../../models/note');
+
+module.exports = {
+  create,
+};
+
+
+async function create(req, res) {
+  try {
+    req.body.user = req.user._id
+
+    const createdNote = await Note.create(req.body);
+    res.json(createdNote);
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err);
+  }
+}
